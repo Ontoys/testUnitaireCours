90058df04a2fcc57817d52fd3807c0f09c701887
ricardo <ricardo.correia.ferreira@gmail.com>
2023-01-31 14:28:20 +0100

    work

--- tests/staff.test.ts
+++ tests/staff.test.ts
@@ -1,19 +1,16 @@
 import test, { ThrowsExpectation } from 'ava';
 import { Staff } from '../src/models/Staff';
+import { StaffType } from '../src/services/types.service';
 
-test('staff should have a valid uuid', (t) => {
-	let err = t.throws(() => {
-		new Staff(
-			'2f0244f0-5b99-422f-8739-62debe85ad63',
-			'jonh',
-			'doe',
-			'jonhdoe@viacesi.fr',
-			'96574289'
-		);
-	});
-	t.is(err?.message, 'Staff ID must be a valid UUID');
-	t.is(err?.message, 'Staff email must be defined');
-	t.is(err?.message, 'Staff first name must be defined');
-	t.is(err?.message, 'Staff last name must be defined');
-	t.is(err?.message, 'Staff phone number must be a valid phone number');
+test('staff should be valid', (t) => {
+	const s = new Staff(
+		'2f0244f',
+		'jonh',
+		'doe',
+		'jonhdoe@viacesi.fr',
+		'0695742890',
+		'password',
+		'pilot' as StaffType
+	);
+	t.is(s.getEmail(), 'jonhdoe@viacesi.fr');
 });
